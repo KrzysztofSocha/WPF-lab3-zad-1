d6cea58bb40963e08f442884a5eea3a6ede5a12e
Krzysztof <krzyssocha27@gmail.com>
2021-05-24 20:17:23 +0200

    wpf lab 3 zadanie 1

---
+++ WpfApp_Lab3_cw_w61933/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Lab3_cw_w61933
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            
+            double x = Convert.ToDouble(tbX.Text);
+            double y = Convert.ToDouble(tbY.Text);
+            tbWynik.Visibility = Visibility.Visible;
+            tbWynik.Text = tbX.Text+ " + " + tbY.Text + " = " + Convert.ToString(x + y);
+
+        }
+
+        private void bRoznica_Click(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(tbX.Text);
+            double y = Convert.ToDouble(tbY.Text);
+            tbWynik.Visibility = Visibility.Visible;
+            tbWynik.Text = tbX.Text + " - " + tbY.Text + " = " + Convert.ToString(x - y);
+        }
+
+        private void bIloczyn_Click(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(tbX.Text);
+            double y = Convert.ToDouble(tbY.Text);
+            tbWynik.Visibility = Visibility.Visible;
+            tbWynik.Text = tbX.Text + " * " + tbY.Text + " = " + Convert.ToString(x * y);
+        }
+
+        private void bIloraz_Click(object sender, RoutedEventArgs e)
+        {
+            double x = Convert.ToDouble(tbX.Text);
+            double y = Convert.ToDouble(tbY.Text);
+            tbWynik.Visibility = Visibility.Visible;
+            if (y != 0)
+            { 
+                tbWynik.Text = tbX.Text + " / " + tbY.Text + " = " + Convert.ToString(x / y);
+            }
+            else
+            {
+                tbWynik.Text = "Nie dziel przez 0";
+            }
+
+        }
+    }
+}
